4b68ad619ea5bc611e093b8b19c3ae490fe26821
yatmadiop <papayatma.diop@gmail.com>
2019-12-07 04:27:47 +0100

    Création de la table + test avec source json chargée via librairie axios

--- clientCancerPrimaire/client_cp/src/App.vue
+++ clientCancerPrimaire/client_cp/src/App.vue
@@ -1,40 +1,55 @@
 <template>
   <div id="app">
     <BarreDeRecherche />
-    <Resultat msg="tftyfg"/>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-
+    <Resultat v-bind:users="users" />
   </div>
-
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-import BarreDeRecherche from './components/BarDeRecherche.vue'
-import Resultat from './components/Resultat.vue'
+import BarreDeRecherche from "./components/BarDeRecherche.vue";
+import Resultat from "./components/Resultat.vue";
 //import 'vue-suggestion/dist/vue-suggestion.css';
 //import Testouille from "./components/Testouille.vue"
-
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    /*HelloWorld,*/
+
     BarreDeRecherche,
-    Resultat,
-    //Testouille
+    Resultat
+
+  },
+  data: function() {
+    return {
+      users: []
+    };
+  },
+  // Librairie axios permet d'utiliser et d'afficher des données provenant d'une API
+  // Test à partir d'un fichier Json répertoriant les utilisateurs d'un site
+  mounted: function() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        this.users = response.data;
+        
+      })
+      .catch(error => {
+        this.errors.push(error);
+      });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin: auto auto;
-  width:75%;
+  width: 75%;
 }
 </style>
--- clientCancerPrimaire/client_cp/src/main.js
+++ clientCancerPrimaire/client_cp/src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 require('../node_modules/vue-suggestion/dist/vue-suggestion.css')
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
